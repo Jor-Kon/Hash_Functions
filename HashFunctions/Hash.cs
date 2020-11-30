6ef90a9696fb738fa09dcebf149645ad129a2ddd
Jordan Kondracki <jordankondracki1999@gmail.com>
2020-11-30 09:15:00 +0100

    added hash tests

---
+++ HashFunctions/Hash.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashFunctions
+{
+    public class Hash
+    {
+        public static string MD5(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            Console.WriteLine(hash);
+            return hash.ToString();
+        }
+
+        public static string SHA1(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+            byte[] bytes = sha1.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            Console.WriteLine(hash);
+            return hash.ToString();
+        }
+
+        public static string SHA2(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            SHA256CryptoServiceProvider sha2 = new SHA256CryptoServiceProvider();
+            byte[] bytes = sha2.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            Console.WriteLine(hash);
+            return hash.ToString();
+        }
+
+        public static string SHA3(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            SHA384CryptoServiceProvider sha3 = new SHA384CryptoServiceProvider();
+            byte[] bytes = sha3.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            Console.WriteLine(hash);
+            return hash.ToString();
+        }
+    }
+}
--- HashFunctions/Program.cs
+++ HashFunctions/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,29 @@ namespace HashFunctions
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+
+            string input = File.ReadAllText(@"C:\Users\JordanK\source\repos\HashFunctions\Hash_Functions\input.txt");
+
+            sw.Start();
+            Hash.MD5(input);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            sw.Start();
+            Hash.SHA1(input);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            sw.Start();
+            Hash.SHA2(input);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            sw.Start();
+            Hash.SHA3(input);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
         }
     }
 }
